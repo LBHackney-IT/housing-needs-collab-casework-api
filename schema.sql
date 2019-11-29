0f034b4228d33e67ea436b5e3cc93fbc527a59bb
Ben Pirt <ben@pirt.co.uk>
2019-11-29 14:44:16 +0000

    Set up a db with docker

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  number VARCHAR(32) NOT NULL,
+  outgoing BOOLEAN,
+  message TEXT,
+  userid VARCHAR(128),
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX messages_number ON messages(number);
