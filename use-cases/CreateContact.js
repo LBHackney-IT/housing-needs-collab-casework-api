0fc293ddf907b252d2b2a87f30620a97edf556d2
Matt Millar <matthew.millar@madetech.com>
2020-03-27 15:01:31 +0000

    Update onboarding message

--- use-cases/CreateContact.js
+++ use-cases/CreateContact.js
@@ -7,9 +7,7 @@ function CreateContact(options) {
 
     if (contact) {
       const onboardingMessages = [
-        'Hackney Council are trying out a new text message service. For the next 6 weeks, you may receive text messages from an officer you’ve spoken with. You can reply to these messages. We expect to reply within 2 working days.',
-        'After 6 weeks we will ask for your feedback. This will help us decide whether or not to continue running the text message service.\nThank you.\nBenefits and Housing Needs Service, Hackney Council.',
-        'If at any point you want to stop receiving text messages, please text STOP.'
+        'If at any point you want to stop receiving text messages from Hackney Council, please text STOP.'
       ];
 
       let systemUser = await dbGateway.getUserByUsername('No Reply');
